9fe868ec17fd37170aa7b846b335cfb381e14177
duza <pavel.mikhadziuk@av.by>
2020-03-16 16:41:26 +0300

    Stylize PostLink as child component of Index page

--- comps/PostLink.js
+++ comps/PostLink.js
@@ -5,6 +5,22 @@ const PostLink = ({ id, name }) => (
     <Link href="/p/[id]" as={`/p/${id}`}>
       <a>{name}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+      
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+      
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 );
 
