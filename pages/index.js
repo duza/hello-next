8c3ab573235c7e0389c7dd06001d4a69226dc816
duza <pavel.mikhadziuk@av.by>
2020-03-16 18:06:09 +0300

    Add filter by author from query string

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import useSWR from 'swr';
+import {useRouter} from 'next/router';
 
 function fetcher(url) {
   return fetch(url).then(r => r.json());
 }
 
 const Index = () => {
-  const { data, error } = useSWR('/api/randomQuote', fetcher);
+  const { query } = useRouter();
+  const { data, error } = useSWR(`/api/randomQuote${query.author ? '?author' + query.author : ''}`, fetcher);
   // The following line has optional chaining, added in Next.js v9.1.5,
   // is the same as `data && data.author`
   const author = data?.author;
