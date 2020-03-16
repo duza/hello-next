06fe887bbae48ac7565e3944bfb01842ae1f3485
duza <pavel.mikhadziuk@av.by>
2020-03-16 12:41:13 +0300

    Add list of links on blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 import Layout from '../comps/MyLayout';
+import PostLink from '../comps/PostLink';
 
-const indexPageContent = <p>Hello Next.js</p>;
+const indexPageContent = (
+  <>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
+  </>
+);
 
-const Index = () => <Layout content={indexPageContent} />;
+const Index = () => (
+  <Layout content={indexPageContent}>
+
+  </Layout>
+);
 
 export default Index;
