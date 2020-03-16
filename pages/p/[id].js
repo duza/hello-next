bc251cc78be9c2cbf99f75299cb4e228a85d5199
duza <pavel.mikhadziuk@av.by>
2020-03-16 17:07:15 +0300

    Add using global styles

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,12 +1,39 @@
 import fetch from 'isomorphic-unfetch';
+import Markdown from 'react-markdown';
 
 import Layout from '../../comps/MyLayout';
 
 const Post = ({ show }) => (
   <Layout>
     <h1>{show.name}</h1>
-    <p>{show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-    {show.image ? <img alt={show.name} src={show.image.medium} /> : null}
+    <div className="markdown">
+      <Markdown
+        source={`
+${show.summary.replace(/<[/]?[pb]>/g, '')}
+${show.image ? `### Poster\n\n![${show.name}](${show.image.medium})` : ''}
+      `}
+      />
+    </div>
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 );
 
