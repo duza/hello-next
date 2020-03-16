b96bbfe3ca1526ec2102795e0aefa364d1082bc1
duza <pavel.mikhadziuk@av.by>
2020-03-16 12:05:19 +0300

    Add Layout component for pages

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,9 @@
-import Header from '../comps/Header';
+import Layout from '../comps/MyLayout';
 
-export default function About() {
-	return (
-		<div>
-			<Header />
-		  <p>This is the about page</p>
-		</div>
-	);
-}
+const About = () => (
+	<Layout>
+		<p>This is the about page</p>
+	</Layout>
+);
+
+export default About;
