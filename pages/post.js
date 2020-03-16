cb21dd7083320c0f1bfba1f6a6b26e649fc05897
duza <pavel.mikhadziuk@av.by>
2020-03-16 12:55:15 +0300

    Move content for post page to separated component

--- pages/post.js
+++ pages/post.js
@@ -2,15 +2,21 @@ import { useRouter } from 'next/router';
 
 import Layout from '../comps/MyLayout';
 
-const Page = () => {
+const Content = () => {
   const router = useRouter();
 
   return (
-    <Layout>
+    <>
       <h1>{router.query.title}</h1>
       <p>This is the blog post content.</p>
-    </Layout>
+    </>
   );
 };
+
+const Page = () => (
+  <Layout>
+    <Content />
+  </Layout>
+);
 
 export default Page;
